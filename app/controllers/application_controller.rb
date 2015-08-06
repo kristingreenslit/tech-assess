ed142691a7e49743d9e594078c7c01f9eceb9bb5
KristinGreenslit <kristingreenslit@gmail.com>
2015-08-06 18:15:38 -0400

    Reenable authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
-  # protect_from_forgery with: :exception
-  #before_action :authenticate_user!
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
 #attempt to redirect to a different page after logging in with devise:
   # def after_sign_in_path_for(resource)
