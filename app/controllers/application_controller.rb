405d51571ab99e224f4caa0f778612dda7559898
KristinGreenslit <kristingreenslit@gmail.com>
2015-08-06 19:56:41 -0400

    MVP / Assessment Form and Assessment Style Changes Complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+
+  # rescue_from 'ActiveRecord::InvalidForeignKey' do
+  # end
+
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
 
 #attempt to redirect to a different page after logging in with devise:
   # def after_sign_in_path_for(resource)
